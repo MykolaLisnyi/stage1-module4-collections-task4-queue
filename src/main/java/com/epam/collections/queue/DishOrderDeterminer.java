d84b581100171f6e2980f01654be65f78c55feef
Mykola Lisnyi <lesnoy.nikolay@gmail.com>
2023-07-27 21:18:24 +0300

    1

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> resultList = new ArrayList<>();
+        Deque<Integer> deque = Stream
+                .iterate(1, i -> i <= numberOfDishes, i -> i + 1)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        while(!deque.isEmpty()) {
+            for (int i = 1; i < everyDishNumberToEat; i++) {
+                deque.addLast(deque.pollFirst());
+            }
+            resultList.add(deque.pollFirst());
+        }
+
+        return resultList;
     }
 }
